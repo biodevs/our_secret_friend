87492e42de559bb8ce010cdb12627bf3bc1a8779
Marcio <marcio@mail.com>
2017-08-05 23:10:06 -0300

    add coffee in member

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,6 @@
 class Member < ApplicationRecord
   belongs_to :campaign
-  after_save :set_campaign_pending
+  after_destroy :set_campaign_pending
   validates :name, :email, presence: true
   
   def set_pixel
