f85b03b26584682dfeafb271e8526f726436393c
Marcio <marcio@mail.com>
2017-08-06 19:15:06 -0300

    raffle template email

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.1'
+gem 'rails', '~> 5.1.2'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
 # Use Puma as the app server
@@ -41,6 +41,8 @@ gem 'materialize-sass'
 gem 'material_icons'
 gem 'redis'
 gem 'sidekiq'
+gem 'inky-rb', require: 'inky'
+gem 'premailer-rails'
 
 group :test do
   gem "factory_girl_rails"
--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,12 @@ module OurSecretFriend
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :sidekiq
     
-    config.time_zone = 'Brasilia'
-    config.encoding = "UTF-8"
-    config.i18n.default_locale = 'pt-BR'
-        
+    # config.time_zone = 'Brasilia'
+    # config.encoding = "UTF-8"
+    # config.i18n.default_locale = 'pt-BR'
+    # config.action_mailer.default_options = {
+    #   charset:       "UTF-8",
+    #   content_type: "text/html"    
+    # }        
   end
 end
